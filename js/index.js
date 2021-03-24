c394aeab3e439e0ddbdec48ec23a452c96a0208a
Filip Tsolov <filip.tsolov@code.berlin>
2021-03-24 14:58:21 +0100

    added js and latest projects grid

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// function changeBgTo(src) {
+//     let bg = document.getElementById("media");
+//     bg.style.backgroundImage = `url(../src/images/previews/${src}.png)`;
+//     console.log(bg.style)
+// }
